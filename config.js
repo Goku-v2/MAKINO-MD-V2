46edbf001a040e525fb3fe0a5f91ede612c37078
Goku-v2 <tddemon1@gmail.com>
2024-07-28 07:18:15 +0100

    Update config.js

--- config.js
+++ config.js
@@ -12,22 +12,22 @@ global.antitags = false;
 
 //auto functioner
 global.autoTyping = false;                //make true to enable auto typing
-global.autoRecord = true;                //make true to enable auto recording
+global.autoRecord = false;                //make true to enable auto recording
 global.groupevent = true;                //This is the new variable for controlling group event handling.
 global.statusseen = false;                 //make true to view statuses 
 
 
-global.Owner = [""];
-global.OwnerNumber = [""];
+global.Owner = ["anos voidigoad"];
+global.OwnerNumber = ["2348117424831"];
 global.ownertag = [""]; 
-global.OwnerName =  "Tᴀɪʀᴀ Mᴀᴋɪɴᴏ";
-global.BotName = "🐦Makino-md-v2";
-global.packname = "Tᴀɪʀᴀ Mᴀᴋɪɴᴏ";                             //Do not change.
-global.author = "🐦Makino md ᴍᴜʟᴛɪ-ᴅᴇᴠɪᴄᴇ";                               //Do not change.
+global.OwnerName =  "anos voldigoad";
+global.BotName = "anos voidigoad";
+global.packname = "★";                             //Do not change.
+global.author = "anos voidigoad ";                               //Do not change.
 global.BotSourceCode = "https://github.com/anonphoenix007/MAKINO-MD-V2"; //Do not change.
-global.SupportGroupLink = "https://chat.whatsapp.com/KEg0G3UUs1G39ikdyfF5Pm "; 
-global.SupportGroupLink = "https://chat.whatsapp.com/KEg0G3UUs1G39ikdyfF5Pm"; //Do not change!
-//global.dbase = process.env.DBASE || "";//Mongodb here
+global.SupportGroupLink = "https://chat.whatsapp.com/E9gwnn6w48dKPLez1BBNt5 "; 
+global.SupportGroupLink = "https://chat.whatsapp.com/E9gwnn6w48dKPLez1BBNt5"; //Do not change!
+//global.dbase = process.env.DBASE || "mongodb+srv://Tboytm:vdUQpHCI0zvK8FW7@cluster0.6aa7wqj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";//Mongodb here
 global.AuthorWhatsapp = "https://wa.me/2347080968564"; //Bot creator WhatsApp link,don't change or bot will not work.
 
 global.sessID = process.env.sessID || ``
@@ -64,17 +64,17 @@ global.ntilink = []
 //
 global.mess = {
     jobdone: 'Here you go...',
-    useradmin: 'Only group Admin can use the command 😂 ',
-    botadmin: 'Make me Admin first 😌📍.',
-    botowner: 'Only my *Owner* can use this command,Dont trespass, Baka!',
+    useradmin: 'Only the demon king can use the command 💀 ',
+    botadmin: 'Make the demon king Admin first 💀.',
+    botowner: 'Only my *demon king* can use this command,Dont trespass, 💀',
     grouponly: 'This command is only made for *Groups*, Baka!',
-    privateonly: 'This command is only made for *Private Chat*, Baka!',
-    botonly: 'Only the *Bot itself* can use this command!',
+    privateonly: 'This command is only made for *Private Chat by the demon king*, 💀',
+    botonly: 'Only the *demon king himself* can use this command!',
     waiting: 'Wait a lil bit (¬_¬)ﾉ...',
     nolink: ' provide me *link*, Baka!',
     error: 'An error occurd!',
-    banned: 'You cant use the commands because you Are *Banned*',
-    bangc: 'This Group is *Banned* from using Commands!',
+    banned: 'You cant use the commands because you Are *Banned* by the demon king💀',
+    bangc: 'This Group is *Banned* from using the demon king Commands!💀',
     nonsfw: 'Dont be a pervert,idiot! This is not a NSFW enabled group!'
     
 }
